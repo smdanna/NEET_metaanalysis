919f858d69d171c80c652115f02116f8546f3126
Sofia <sofia.m.danna@gmail.com>
2021-03-19 16:08:58 -0400

    Update rve.R

--- rve.R
+++ rve.R
@@ -23,57 +23,22 @@ neet <- read.csv("~/Dropbox/PEPP/NEET meta-analysis/metaanalysis/neet 20201027.c
 
 
 
-
-
-#### fit RVE random-effects model with correlated effects weights ####
-
-
+#### inspect the data ####
 # create violin plot to visualize the plot
 str(neet)
+View(neet)
 
 vioplot(neet$OR_ln, col="grey", names="NEET mental health associations")
 title(ylab="log OR")
 
 
+
+# fit RVE random-effects model with correlated effects weights
+
 # rho assumed = 0.8; sensitivity analyses could vary rho from 0.1 to 0.9 
 # small = TRUE option applies the small sample correction to df 
-re.all<-robu(formula = OR_ln ~ 1, var.eff.size=SE, studynum = Study, 
-            modelweights = "CORR", rho = 0.8, small=TRUE, data=neet) 
-print(re.all)
 
-
-
-
-#### testing using distress as example ####
-dis <- filter(neet, Distress==1 & is.na(Repeated_sample)==TRUE & is.na(rep_dis)==TRUE & !is.na(OR_ln))
-dis
-View(dis)
-
-dis.full <- filter(neet, Distress==1 & !is.na(OR_ln))
-dis.full
-View(dis.full)
-
-vioplot(dis.full$OR_ln, col="grey", names="NEET-MH association")
-
-rve.dis.full<-robu(formula = OR_ln ~ 1, var.eff.size=SE, studynum = Study, 
-             modelweights = "CORR", rho = 0.8, small=TRUE, data=dis.full) 
-print(rve.dis.full)
-
-# forest plot
-# 1 default
-forest.robu(rve.dis.full, es.lab = "Mental.health.measures", study.lab = "Study",
-            "OR_ln" = effect.size, # optional column
-            "Weight" = r.weights)        # optional column
-
-# 2 
-forest.robu(rve.dis.full, es.lab = "Mental.health.measures", study.lab = "Study",
-            "log OR" = effect.size, "Weight" = r.weights, "OR" = OR)
-
-View(neet)
-
-
-
-#### mental health measure ####
+## mental health measures -------------------------------------------------------------------------------
 
 # creating deduplicated subsets for main analysis
 dis <- filter(neet, Distress==1 & is.na(Repeated_sample)==TRUE & is.na(rep_dis)==TRUE & !is.na(OR_ln))
@@ -91,26 +56,32 @@ beh
 sui <- filter(neet, Suicidal==1 & Gender=="all" & is.na(Repeated_sample))
 sui
 
-# binding together
+# binding together into one measure
 mh <- bind_rows(dis, mood, anx, beh, sui)
 mh
 
-# rve random effects model
+# rve random effects model with correlated effects weights
 mh.rve<-robu(formula = OR_ln ~ 1, var.eff.size=SE, studynum = Study, 
                    modelweights = "CORR", rho = 0.8, small=TRUE, data=mh) 
 print(mh.rve)
 
+# exponentiate to obtain OR and CI
 exp(0.273)
 
 exp(0.0704)
 exp(0.476)
 
+# show more of what's going on 
+summary(su.rve)
+str(su.rve)  
+
 # forest plot
 forest.robu(mh.rve, es.lab = "Mental.health.measures", study.lab = "Study",
             "log OR" = effect.size, # optional column
             "Weight" = r.weights)   # optional column
+dev.off()
 
-#### substance use measure ####
+## substance use measures -------------------------------------------------------------------------------
 
 # creating deduplicated subsets for main analysis
 alc <- filter(neet, Alcohol==1 & Gender=="all" & is.na(Repeated_sample) & !is.na(OR_ln))
@@ -131,6 +102,7 @@ su.rve<-robu(formula = OR_ln ~ 1, var.eff.size=SE, studynum = Study,
              modelweights = "CORR", rho = 0.8, small=TRUE, data=su) 
 print(su.rve)
 
+# exponentiate to obtain OR and CI
 exp(0.359)
 
 exp(0.0794) 
@@ -142,15 +114,13 @@ forest.robu(su.rve, es.lab = "Mental.health.measures", study.lab = "Study",
             "Weight" = r.weights)   # optional column
 dev.off()
 
-# alternative plot, includes N
+# forest plot, includes N
 forest.robu(su.rve, es.lab = "Mental.health.measures", study.lab = "Study",
-            "log OR" = effect.size, "Weight" = r.weights, "n" = N)
+            "log OR" = effect.size, "Weight" = r.weights, 
+            "n" = N)
 dev.off()
 
-# shows all the model output 
-str(su.rve)  
-
-#### overall measure ####
+## all measures -------------------------------------------------------------------------------
 
 # creating deduplicated subsets for main analysis
 any <- filter(neet, Any==1 & Gender=="all" & is.na(rep_any))
@@ -165,6 +135,7 @@ all.rve<-robu(formula = OR_ln ~ 1, var.eff.size=SE, studynum = Study,
              modelweights = "CORR", rho = 0.8, small=TRUE, data=all) 
 print(all.rve)
 
+# exponentiate to obtain OR and CI
 exp(0.321)        
 
 exp(0.144) 
@@ -174,4 +145,4 @@ exp(0.497)
 forest.robu(all.rve, es.lab = "Mental.health.measures", study.lab = "Study",
             "log OR" = effect.size, # optional column
             "Weight" = r.weights)   # optional column
-
+dev.off()
